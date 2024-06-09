82a67896c28d6cb4bd42ee985fb1b3db06a7ef26
Đình Khoa <ndk1904@gmail.com>
2024-06-09 22:44:02 +0700

    character_count
    Câu hỏi 2:(Code) Hoàn thành chương trình sau với mô tả bài toán từ câu I.2. Đầu ra của chương trình
    dưới đây là gì?
    1 def character_count ( word ) :
    2 character_statistic = {}
    3
    4 # Your Code Here
    5
    6 # End Code Here
    7 return character_statistic
    8
    9 assert character_count (" Baby ") == {’B’: 1 , ’a’: 1 , ’b’: 1 , ’y’: 1}
    10 print ( character_count (’smiles ’) )
    a) ’s’: 2, ’m’: 1, ’i’: 1, ’l’: 1, ’e’: 1
    b) ’s’: 0, ’m’: 1, ’i’: 1, ’l’: 1, ’e’: 8
    c) ’s’: 4, ’m’: 1, ’i’: 2, ’l’: 1, ’e’: 1
    d) Raise a Error

---
+++ EX02/Cau2_dictionary.py
@@ -0,0 +1,9 @@
+def character_count (word):
+    character_statistic={}
+    for char in word:
+        if char in character_statistic:
+            character_statistic[char]+=1
+        else:
+            character_statistic[char]=1
+    return character_statistic
+print(character_count('smiles'))
