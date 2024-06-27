57533db00c52cea53016c59db8179f1069673cd2
Đình Khoa <ndk1904@gmail.com>
2024-06-27 21:48:24 +0700

    test sonar trigger

--- M01EC03 - Classes and Objects/Lesson.py
+++ M01EC03 - Classes and Objects/Lesson.py
@@ -1,22 +1,33 @@
+"test"
+
+
 class Y:
-    def __init__(self,year,month,day):
-        self.__year=year
-        self.__month=month
-        self.__day=day
+    def __init__(self, year, month, day):
+        self.__year = year
+        self.__month = month
+        self.__day = day
+
     def get_year(self):
         return self.__year
-    def set_year(self,year):
-        self.__year= year
+
+    def set_year(self, year):
+        self.__year = year
+
     def get_month(self):
         return self.__month
-    def set_month(self,month):
-        self.__month=month
+
+    def set_month(self, month):
+        self.__month = month
+
     def get_day(self):
         return self.__day
-    def set_day(self,day):
-        self.__day=day
-    def describe (self):
+
+    def set_day(self, day):
+        self.__day = day
+
+    def describe(self):
         print(self.__year)
 
-d=Y(2000,1,2)
+
+d = Y(2000, 1, 2)
 print(d.get_year())
